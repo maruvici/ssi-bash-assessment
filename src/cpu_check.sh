005419adc4eead0805f60b742ce8a4d4461be003
Mav Evangelista <mavevangelista@localhost.localdomain>
2025-11-08 00:05:00 +0800

    feat: Add email send & check logic to cpu_check

--- src/cpu_check.sh
+++ src/cpu_check.sh
@@ -24,7 +24,7 @@ while getopts ":c:w:e:" param; do
             echo "Error: -$OPTARG requires an argument"; exit 4
             ;;
         \?)
-            echo "Usage: bash memory_check.sh -c argC -w argW -e argE"; exit 4
+            echo "Usage: bash cpu_check.sh -c argC -w argW -e argE"; exit 4
             ;;
     esac
 done
@@ -33,7 +33,7 @@ PERCENTAGE_REGEX="^[0-9]+$"
 EMAIL_REGEX="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 
 if [ $(( $c_set + $w_set + $e_set )) -ne 0 ]; then
-    echo "Usage: memory_check.sh -c argC -w argW -e argE"; exit 4
+    echo "Usage: cpu_check.sh -c argC -w argW -e argE"; exit 4
 elif ! [[ $crit_thresh =~ $PERCENTAGE_REGEX && $crit_thresh -le 100 && $crit_thresh -ge 0 ]]; then
     echo "Enter valid critical threshold percentage"; exit 4
 elif ! [[ $warn_thresh =~ $PERCENTAGE_REGEX && $warn_thresh -le 100 && $warn_thresh -ge 0 ]]; then
@@ -45,3 +45,16 @@ elif ! [[ $email =~ $EMAIL_REGEX ]]; then
 fi
 
 # CPU CHECK
+total_cpu_used=$( top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}' )
+
+if [[ $( echo "$total_cpu_used < $warn_thresh" | bc ) = 1 ]]; then
+    exit 0
+elif [[ $( echo "($total_cpu_used >= $warn_thresh) * ($total_cpu_used < $crit_thresh)" | bc ) = 1 ]]; then
+    exit 1
+elif [[ $(echo "($total_cpu_used >= $crit_thresh)" | bc ) = 1 ]]; then
+    subject="$(date +"%Y%m%d %H:%M ") cpu_check - critical" 
+    body="$(top -b -n 1 -o %CPU | head -n 17 | tail -n 11)"
+    message="CPU usage is now critical. Below are the top 10 processes in terms of CPU use:\n$body"
+    echo -e "$message" | mailx -s "$subject" $email
+    exit 2
+fi
